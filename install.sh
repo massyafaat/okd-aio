d5506720a1a319b7c3f8f07b5ddf536fc280224e
root <root@okd-aio.azizpunya.com>
2019-12-20 18:19:38 +0700

    first commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+mkdir -p /etc/origin/master/
+htpasswd -b /etc/origin/master/htpasswd admin admin123
+git clone https://github.com/openshift/openshift-ansible.git -b release-3.11 --depth=1
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
+oc adm policy add-cluster-role-to-user cluster-admin admin
