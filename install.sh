d5c8dbbdb20bb893a87c036d1f5ca050e568ee17
abdulazizms <57166632+abdulazizms@users.noreply.github.com>
2019-12-20 18:26:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 mkdir -p /etc/origin/master/
+touch /etc/origin/master/htpasswd
 htpasswd -b /etc/origin/master/htpasswd admin admin123
 git clone https://github.com/openshift/openshift-ansible.git -b release-3.11 --depth=1
 ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
