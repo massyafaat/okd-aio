f9fdde8bd740f3c6acaafe6b2b948234a2f7bd7d
root <root@okd-aio.azizpunya.com>
2019-12-20 18:17:04 +0700

    first commit

--- install-tools.sh
+++ install-tools.sh
@@ -25,4 +25,4 @@ yum -y --enablerepo=epel install pyOpenSSL
 
 # install Ansible
 
-yum -y --enablerepo=epel install https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
+yum -y --enablerepo=epel install ansible
